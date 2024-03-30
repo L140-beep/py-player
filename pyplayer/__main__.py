7fbb1a107c6298cefb8323f46cbe84e8f11d0e2f
L140-beep <kartoshka0302@mail.ru>
2024-03-30 21:57:33 +0800

    pause/play/resume

--- pyplayer/__main__.py
+++ pyplayer/__main__.py
@@ -1,5 +1,52 @@
 """Main module."""
+
+import sys
+from threading import Thread, Event
+
+from PyQt5.QtGui import QCloseEvent
+
 from .audioplayer import AudioPlayer
+from .interfaces.mainwindow import Ui_MainWindow
+
+from PyQt5 import QtWidgets
+
+
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.play_button.clicked.connect(self.clickedPlayBtn)
+        self.ui.pause_button.clicked.connect(self.clickedPauseBtn)
+        self.killThread = Event()
+        self.player = AudioPlayer()
+
+    def createAudioThread(self):
+        return Thread(target=self.playAudio,
+                      args=(self.killThread,))
+
+    def playAudio(self, killPill: Event):
+        self.killThread.clear()
+        self.player.play('demos/Korol_i_SHut_-_Kukla_kolduna_62570545.mp3', killPill)
+
+    def clickedPauseBtn(self):
+        self.killThread.set()
+
+    def clickedPlayBtn(self):
+        self.audio_thread = self.createAudioThread()
+        self.audio_thread.start()
+
+    def closeEvent(self, a0: QCloseEvent | None) -> None:
+        self.killThread.set()
+        return super().closeEvent(a0)
+    # TODO: убить тред
+
 
 if __name__ == '__main__':
-    AudioPlayer().play('demos/Radiohead_-_Creep_48022418.mp3')
+    app = QtWidgets.QApplication([])
+    # main_window = uic.loadUi('pyplayer/interfaces/MainWindow.ui')
+    # main_window.show()
+    win = mywindow()
+    win.show()
+    app.exec()
+    sys.exit(1)
--- pyplayer/audioplayer.py
+++ pyplayer/audioplayer.py
@@ -1,13 +1,53 @@
 """Module implements music playback."""
 
+from threading import Event
 from pydub import AudioSegment
-from pydub.playback import play
+from pydub.playback import play, _play_with_simpleaudio
+import pyaudio
+from typing import Protocol
+
+
+class Music(Protocol):
+    sample_width: int
+    frame_rate: int
+    channels: int
+    frame_width: int
+    raw_data: bytes
+
+
+CHUNK = 100000
 
 
 class AudioPlayer:
     """Class to interact with audio."""
 
-    def play(self, path: str) -> None:
+    def __init__(self) -> None:
+        self.playback = None
+        self.p = pyaudio.PyAudio()
+        self.last = 0
+
+    def play(self, path: str, killPill: Event) -> None:
         """Play music."""
-        music = AudioSegment.from_mp3(path)
-        play(music)
+        try:
+            music: Music = AudioSegment.from_mp3(path)
+            # sample_width, frame_rate, channels, frame_width, _data
+            self.stream = pyaudio.Stream(
+                PA_manager=self.p,
+                format=pyaudio.get_format_from_width(music.sample_width),
+                channels=music.channels,
+                rate=music.frame_rate,
+                output=True
+            )
+            for i in range(self.last, len(music.raw_data), CHUNK):
+                if killPill.is_set():
+                    print('hertrr')
+                    self.last = i
+                    self.pause()
+                    break
+                self.stream.write(music.raw_data[i:i + CHUNK])
+        except Exception:
+            return
+
+    def pause(self) -> None:
+        self.stream.close()
+        self.stream.stop_stream()
